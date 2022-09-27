5402fbb57cb9e75500c45773eb3e5ef396da4f96
CrazyReborn <vladkuzodin@gmail.com>
2022-09-27 17:22:19 +0200

    feat: added sanitization to create-report dto.

--- src/dto/create-report.dto.ts
+++ src/dto/create-report.dto.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
+import * as sanitizeHtml from 'sanitize-html';
 
 export class CreateReportDto {
   @ApiProperty()
@@ -19,5 +21,6 @@ export class CreateReportDto {
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => sanitizeHtml(value))
   text: string;
 }
